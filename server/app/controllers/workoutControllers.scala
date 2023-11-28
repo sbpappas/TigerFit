91bc39acf055a6f241c77ef94227df942ddbf0c9
Samuel <Samuel@Samuels-MacBook-2.local>
2023-11-28 14:28:56 -0600

    added controllers

---
+++ server/app/controllers/workoutControllers.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import models.memoryModelFit
+import javax.inject._
+import play.api.mvc._
+import play.api.i18n._
+import java.lang.ProcessBuilder.Redirect
+
+@Singleton
+class workoutController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+}
--- server/app/models/memoryModelFit.scala
+++ server/app/models/memoryModelFit.scala
@@ -6,6 +6,7 @@ object memoryModelFit{
     private var userPass = mutable.Map[String, String]("samc" -> "drowssap", "setho" -> "password", "oliviab" -> "123", "samuelp"-> " ") 
     private val goals = Seq[String] ("Gain Muscle", "Lose Weight", "Stay Healthy")
     private var userInfo = mutable.Map[String, Tuple4[Int, Int, String, Int]]("samc" -> Tuple4(180, 72, goals(0), 4))
+     
 
     def validateUser(username: String, password: String): Boolean = {
         userPass.get(username).map(_ == password).getOrElse(false)
@@ -25,6 +26,7 @@ object memoryModelFit{
         }
         else false
     }
+
 
     // def updateWeight(username: String, newWeight: String): Boolean = {
     //     userInfo(username)._1 = newWeight
